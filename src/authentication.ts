42f13493fc4a1cf9bfb4f910ab3b62052d9040f0
Siddharth Trivedi <shtrived@gmail.com>
2020-04-26 00:24:41 -0700

    Re-create authentication service

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,6 +1,5 @@
 import { ServiceAddons } from '@feathersjs/feathers';
 import { AuthenticationService, JWTStrategy } from '@feathersjs/authentication';
-import { LocalStrategy } from '@feathersjs/authentication-local';
 import { expressOauth } from '@feathersjs/authentication-oauth';
 
 import { Application } from './declarations';
@@ -15,7 +14,6 @@ export default function(app: Application) {
   const authentication = new AuthenticationService(app);
 
   authentication.register('jwt', new JWTStrategy());
-  authentication.register('local', new LocalStrategy());
 
   app.use('/authentication', authentication);
   app.configure(expressOauth());
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,7 @@
 import { Application } from '../declarations';
-// import null from './null/null.service';
+import users from './users/users.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application) {
-  // app.configure(null);
+  app.configure(users);
 }
