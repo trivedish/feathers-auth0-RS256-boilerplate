993742f14aa6cc5b36f4506b294a89acf609c2f3
Siddharth Trivedi <sid_heart77@hotmail.com>
2020-05-31 16:58:33 -0700

    Create authentication hook.

--- src/hooks/authenticate.ts
+++ src/hooks/authenticate.ts
@@ -1,5 +1,3 @@
-// Use this hook to manipulate incoming or outgoing data.
-// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
 import errors from '@feathersjs/errors';
 import jwt from 'jsonwebtoken';
